ac566cc46d5f41a9a30d7314ab24229d94044415
Dmitriy <informatika.1827@gmail.com>
2024-12-23 09:06:11 +0400

    server api link

--- pages/admin.vue
+++ pages/admin.vue
@@ -26,7 +26,7 @@ const exitAdmin = () => {
 
 const fetchAdmin = async () => {
   try {
-    await axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie', {withCredentials: true});
+    await axios.get('https://api.solargy.shop/sanctum/csrf-cookie', {withCredentials: true});
     const response = await axios.post(`/login?email=${nameUser.value}&password=${passwordUser.value}`, {withCredentials: true});
     result.value = response.data;
     isAuthenticated.value = true;
